d7f4b06f8401fac5654d7437aecdac9409fc62f8
Gabriel Albinet <gabriel.albinet.pro@gmail.com>
2026-07-23 16:03:05 +0200

    appel des produits via API et affichage dans les cartes

--- src/app/inscription/inscription.ts
+++ src/app/inscription/inscription.ts
@@ -1,16 +1,26 @@
 import { Component } from '@angular/core';
 import { Router, RouterLink } from '@angular/router';
+import { FormsModule } from '@angular/forms';
+import { AuthService } from '../services/auth';
 
 @Component({
   selector: 'app-inscription',
-  imports: [RouterLink],
+  imports: [RouterLink, FormsModule],
   templateUrl: './inscription.html',
-  styleUrl: './inscription.css',
+  styleUrl: './inscription.css'
 })
 export class Inscription {
-  constructor(private router: Router) {}
+  email = '';
+  password = '';
+  confirmPassword = '';
+
+  constructor(private router: Router, private authService: AuthService) {}
 
   onCreerCompte() {
+    if (this.password !== this.confirmPassword) {
+      return;
+    }
+    this.authService.register(this.email, this.password);
     this.router.navigate(['/login']);
   }
 }
