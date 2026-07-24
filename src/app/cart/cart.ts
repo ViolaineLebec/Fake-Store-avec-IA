4f6506e2d480ff748a6a3b2d12c33dea708ba5d9
Violaine L <vlebec@proton.me>
2026-07-24 10:46:36 +0200

    Page de remerciement

--- src/app/cart/cart.ts
+++ src/app/cart/cart.ts
@@ -2,6 +2,7 @@ import { Component, inject } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { SelectedCard } from '../selected-card/selected-card';
 import { Cart as CartService } from '../services/cart';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-cart',
@@ -11,4 +12,10 @@ import { Cart as CartService } from '../services/cart';
 })
 export class Cart {
   cartService = inject(CartService);
+  private router = inject(Router);
+
+  passerCommande(): void {
+    this.router.navigate(['/merci']);
+  }
 }
+
