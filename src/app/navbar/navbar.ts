b7268ca26eff6be71ba2c25fdb626217b7b22e3b
Gabriel Albinet <gabriel.albinet.pro@gmail.com>
2026-07-24 10:57:43 +0200

    Pagination faite, searchbar et indice de connexion

--- src/app/navbar/navbar.ts
+++ src/app/navbar/navbar.ts
@@ -1,10 +1,14 @@
 import { Component } from '@angular/core';
 import { RouterLink, RouterLinkActive } from '@angular/router';
+import { Auth } from '../services/auth';
 
 @Component({
   selector: 'app-navbar',
   imports: [RouterLink, RouterLinkActive],
   templateUrl: './navbar.html',
-  styleUrl: './navbar.css',
+  styleUrl: './navbar.css'
 })
-export class Navbar {}
+export class Navbar {
+  constructor(public auth: Auth) {}
+}
+
